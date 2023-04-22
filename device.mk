5c530f99ba405c0b707c25ae65ef2cdb0fb8ae57
David112x <86davidx64@gmail.com>
2023-04-21 23:50:45 -0500

    some fixes idk

--- device.mk
+++ device.mk
@@ -7,10 +7,6 @@
 # Inherit from the proprietary configuration
 $(call inherit-product, vendor/xiaomi/surya/surya-vendor.mk)
 
-# AAPT
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
