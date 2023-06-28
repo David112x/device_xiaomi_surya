a585b3372e18b81e3c556f3b7ee7b85cc78c6a18
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-06-28 17:28:24 +0000

    Revert "surya: Implement extension for kernelspace battery saver"
    This reverts commit 3e14cb9e20df911ff890fcb67d2702e516c71ef2.
    
    Change-Id: I27421c512753b7d5e5364c500a83362480c80ae8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,8 +114,7 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
-TARGET_TAP_TO_WAKE_NODE  := "/proc/tp_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Properties
 #TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
